42b269946b251b983bc04d0f9c087dfee12c1934
Darshu1221 <111422864+Darshu1221@users.noreply.github.com>
2022-10-20 12:34:19 +0530

    Update Even_Ascending_Array.java

--- Even_Ascending_Array.java
+++ Even_Ascending_Array.java
@@ -6,12 +6,13 @@
 
 
 import java.util.Arrays;
-import java.util.Scanner;//importing scanner class
+import java.util.Scanner;
 public class Even_Ascending_Array{
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);//creating object of scanner class
+		Scanner sc = new Scanner(System.in);
+               //creating an object of scanner class
 		System.out.print("Enter the number of element in array:");
 		int size=sc.nextInt();
 		
@@ -21,14 +22,14 @@ public class Even_Ascending_Array{
 		{
 			a[i]=sc.nextInt();
 		}
-		// for loop for checking even number in array
+		//checking an even number in array
 		for(int i=0;i<a.length;i++)
 		{  
 			if(a[i]%2==0) 
 			System.out.println("Even Number are:"+a[i]);
 			
 		}
-		//to arrange array in ascending order we use sort method 
+		
 		Arrays.sort(a);
 		System.out.println("\n Ascending order of array:");
 		for (int n:a)
