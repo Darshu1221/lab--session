7d22f09cba7780be0c9f4d426bd5ac9cd106db1f
Darshu1221 <111422864+Darshu1221@users.noreply.github.com>
2022-10-20 12:38:48 +0530

    Update ArraySorting.java

--- ArraySorting.java
+++ ArraySorting.java
@@ -1,4 +1,4 @@
-/*
+Hb/*
  * Wap to print ascending and descending order of an array
  * Date:-20/10/22
  * Name:- Darshana Parab
@@ -14,10 +14,12 @@ public class ArraySorting {
         for (i = 0; i < 5; i++) {
             arr[i] = sc.nextInt();
         }
-
+        
+        //printing numbers in Ascending order
         Arrays.sort(arr);
         System.out.println("Sorted Array in Ascending Order " + Arrays.toString(arr));
-
+        
+        // printing numbers in Descending order
         Arrays.sort(arr,Comparator.reverseOrder());
         System.out.println("Sorted Array in Descending Order " + Arrays.toString(arr));
     }
