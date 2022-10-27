0cd7ed29d2ea1481cb88faac3d25a89a6c5519b2
Darshu1221 <111422864+Darshu1221@users.noreply.github.com>
2022-10-27 11:17:48 +0530

    Create Diwali.java

---
+++ Diwali.java
@@ -0,0 +1,13 @@
+public class Diwali
+{
+    public static void main(String[] args) 
+{
+        String name = " Happy Diwali ";
+        
+        for (int i = 0; i < name.length(); i++) {
+           
+        System.out.print(name.charAt(i));
+        }
+        
+    }
+}
